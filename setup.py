d50ee27ed6fc926bf7964efbf7312d9cfbcb0231
DAME RAJEE <doss72180@gmail.com>
2024-10-23 15:18:24 +0000

    update init

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="triformer",
-    version="0.0.6",
+    version="0.0.8",
     author="DameRajee",
     author_email="doss72180@gmail.com",
     description="A Transformer implementation using Triton for GPU acceleration",
