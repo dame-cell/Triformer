0730a2c469246c945b37449b5bcfd745d07234aa
win10 <doss72180@gmail.com>
2024-10-26 13:20:21 +0530

    added the use_relu parameter

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'triformer',
   packages = find_packages(),
-  version = '1.1.1',
+  version = '1.2.0',
   license='MIT',
   description = 'Transformer components in Triton',
   author = 'Dame rajee',
