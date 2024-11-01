c2064af4647b66b896c1aae82271ece768608042
win10 <doss72180@gmail.com>
2024-11-01 19:48:12 +0530

    update the dropout

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'triformer',
   packages = find_packages(),
-  version = '3.0.1',
+  version = '3.0.2',
   license='MIT',
   description = 'Transformer components in Triton',
   long_description='This package implements transformer components in Triton.',
