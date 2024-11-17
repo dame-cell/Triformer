bdb410c70126e24af39f9e48cee821c884e1b3af
cell-dame <122996026+dame-cell@users.noreply.github.com>
2024-11-17 18:52:28 +0530

    faster backward for swiglu

--- triformer/swiglu.py
+++ triformer/swiglu.py
@@ -1,141 +1,108 @@
 import triton 
 import triton.language as tl
 import torch 
-from .utils import calculate_settings
-
 
 @triton.jit
 def swiglu_forward_optimized(
     e_ptr, g_ptr, output_ptr,
+    sigmoid_ptr, f_ptr, 
     e_stride, g_stride, output_stride,
+    sigmoid_stride, f_stride, 
     BLOCK_SIZE: tl.constexpr,
     n_cols,
-    num_warps: tl.constexpr
 ):
     row_idx = tl.program_id(axis=0)
-    col_offset = tl.arange(0, BLOCK_SIZE)
+    col_offset = tl.arange(0, BLOCK_SIZE)  
     mask = col_offset < n_cols
 
     e_ptr += row_idx * e_stride
     g_ptr += row_idx * g_stride
     output_ptr += row_idx * output_stride
+    sigmoid_ptr += row_idx * sigmoid_stride
+    f_ptr += row_idx * f_stride
 
     e_row = tl.load(e_ptr + col_offset, mask=mask).to(tl.float32)
     g_row = tl.load(g_ptr + col_offset, mask=mask).to(tl.float32)
-    
+
     sigmoid_e_row = tl.sigmoid(e_row)
     f_row = e_row * sigmoid_e_row
+    
+    tl.store(sigmoid_ptr + col_offset, sigmoid_e_row, mask=mask)
+    tl.store(f_ptr + col_offset, f_row, mask=mask)
+    
     output_row = f_row * g_row
-
     tl.store(output_ptr + col_offset, output_row, mask=mask)
 
-@triton.jit
-def swiglu_backward_optimized(
-    dy_ptr, e_ptr, g_ptr,
-    grad_e_ptr, grad_g_ptr,
-    dy_stride, e_stride, g_stride,
-    grad_e_stride, grad_g_stride,
+
+@triton.jit 
+def swiglu_backward(
+    grad_output_ptr, grad_e_ptr, grad_g_ptr, 
+    e_ptr, g_ptr, n_cols, sigmoid_ptr, f_ptr,
+    grad_output_stride, grad_e_stride, grad_g_stride,
+    e_stride, g_stride, sigmoid_stride, f_stride,
     BLOCK_SIZE: tl.constexpr,
-    n_cols,
-    num_warps: tl.constexpr
 ):
-    row_idx = tl.program_id(axis=0)
+    pid = tl.program_id(axis=0)
     col_offset = tl.arange(0, BLOCK_SIZE)
     mask = col_offset < n_cols
 
-    # Calculate pointers
-    dy_ptr += row_idx * dy_stride
-    e_ptr += row_idx * e_stride
-    g_ptr += row_idx * g_stride
-    grad_e_ptr += row_idx * grad_e_stride
-    grad_g_ptr += row_idx * grad_g_stride
+    grad_output_row = tl.load(grad_output_ptr + pid * grad_output_stride + col_offset, mask=mask)
+    e_row = tl.load(e_ptr + pid * e_stride + col_offset, mask=mask)
+    g_row = tl.load(g_ptr + pid * g_stride + col_offset, mask=mask)
+    sigmoid_row = tl.load(sigmoid_ptr + pid * sigmoid_stride + col_offset, mask=mask)
+    f_row = tl.load(f_ptr + pid * f_stride + col_offset, mask=mask)
 
-    # Load inputs
-    dy = tl.load(dy_ptr + col_offset, mask=mask).to(tl.float32)
-    e = tl.load(e_ptr + col_offset, mask=mask).to(tl.float32)
-    g = tl.load(g_ptr + col_offset, mask=mask).to(tl.float32)
+    grad_g_row = grad_output_row * f_row
+    
+    grad_e_row = grad_output_row * g_row * sigmoid_row * (1.0 + e_row * (1.0 - sigmoid_row))
 
-    # Compute intermediates on the fly
-    sigmoid_e = tl.sigmoid(e)
-    f = e * sigmoid_e
-
-    # Compute gradients
-    grad_g = dy * f
-    grad_e = dy * g * sigmoid_e * (1.0 + e * (1.0 - sigmoid_e))
-
-    # Store results
-    tl.store(grad_e_ptr + col_offset, grad_e, mask=mask)
-    tl.store(grad_g_ptr + col_offset, grad_g, mask=mask)
+    tl.store(grad_e_ptr + pid * grad_e_stride + col_offset, grad_e_row, mask=mask)
+    tl.store(grad_g_ptr + pid * grad_g_stride + col_offset, grad_g_row, mask=mask)
 
 class FastSwigluFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx, e, g):
-        e = e.contiguous()
-        g = g.contiguous()
-        
-        assert e.shape == g.shape, "Input tensors must have the same shape"
-        
-        shape = e.shape
-        dim = shape[-1]
-        e_reshaped = e.view(-1, dim)
-        g_reshaped = g.view(-1, dim)
-        n_rows, n_cols = e_reshaped.shape
-
-        BLOCK_SIZE, num_warps = calculate_settings(n_cols)
-        
+        n_cols = g.shape[-1]
+        sigmoid_ptr = torch.empty_like(e)
+        f_ptr = torch.empty_like(e)
         output = torch.empty_like(e)
-        output_reshaped = output.view(-1, dim)
 
-        grid = (n_rows,)
+        # Calculate BLOCK_SIZE
+        BLOCK_SIZE = triton.next_power_of_2(n_cols)
+        BLOCK_SIZE = min(max(BLOCK_SIZE, 32), 1024)
+
+        grid = (e.shape[0] * e.shape[1],)
         swiglu_forward_optimized[grid](
-            e_reshaped, g_reshaped, output_reshaped,
-            e_reshaped.stride(0), g_reshaped.stride(0), output_reshaped.stride(0),
-            BLOCK_SIZE,
-            n_cols,
-            num_warps
+            e, g, output,
+            sigmoid_ptr, f_ptr,
+            e.stride(1), g.stride(1), output.stride(1),
+            sigmoid_ptr.stride(1), f_ptr.stride(1),
+            BLOCK_SIZE=BLOCK_SIZE,
+            n_cols=n_cols,
         )
 
-        ctx.save_for_backward(e, g)
-        ctx.shape = shape
+        ctx.save_for_backward(e, g, sigmoid_ptr, f_ptr)
         ctx.BLOCK_SIZE = BLOCK_SIZE
-        ctx.num_warps = num_warps
-
+        ctx.n_cols = n_cols
         return output
 
     @staticmethod
     def backward(ctx, grad_output):
-        grad_output = grad_output.contiguous()
-        e, g = ctx.saved_tensors
-        shape = ctx.shape
+        e, g, sigmoid_ptr, f_ptr = ctx.saved_tensors
         BLOCK_SIZE = ctx.BLOCK_SIZE
-        num_warps = ctx.num_warps
-
-        dim = shape[-1]
-        grad_output_reshaped = grad_output.view(-1, dim)
-        e_reshaped = e.view(-1, dim)
-        g_reshaped = g.view(-1, dim)
+        n_cols = ctx.n_cols
 
         grad_e = torch.empty_like(e)
         grad_g = torch.empty_like(g)
-        grad_e_reshaped = grad_e.view(-1, dim)
-        grad_g_reshaped = grad_g.view(-1, dim)
 
-        n_rows, n_cols = e_reshaped.shape
-
-        grid = (n_rows,)
-        swiglu_backward_optimized[grid](
-            grad_output_reshaped, e_reshaped, g_reshaped,
-            grad_e_reshaped, grad_g_reshaped,
-            grad_output_reshaped.stride(0), 
-            e_reshaped.stride(0), 
-            g_reshaped.stride(0),
-            grad_e_reshaped.stride(0), 
-            grad_g_reshaped.stride(0),
-            BLOCK_SIZE,
-            n_cols,
-            num_warps
+        grid = (e.shape[0] * e.shape[1],)
+        swiglu_backward[grid](
+            grad_output.contiguous(), grad_e, grad_g,
+            e, g, n_cols, sigmoid_ptr, f_ptr,
+            grad_output.stride(1), grad_e.stride(1), grad_g.stride(1),
+            e.stride(1), g.stride(1), sigmoid_ptr.stride(1), f_ptr.stride(1),
+            BLOCK_SIZE=BLOCK_SIZE
         )
-
         return grad_e, grad_g
 
 class TritonSwiglu(torch.nn.Module):
